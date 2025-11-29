57a6a2a9878d1515f88d338ff1b16f17b22b3b4c
ECortus <ve.cortus@gmail.com>
2025-11-30 01:22:15 +0200

    remove useless

--- Assets/CachingObjectsTool/Editor/Modules/OverrideObjectPropertyDrawerModule.cs
+++ Assets/CachingObjectsTool/Editor/Modules/OverrideObjectPropertyDrawerModule.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Threading;
-using CachingObjectsTool.Editor.Common;
+﻿using CachingObjectsTool.Editor.Common;
 using CachingObjectsTool.Runtime;
 using Cysharp.Threading.Tasks;
 using UnityEditor;
@@ -8,26 +6,27 @@ using UnityEngine;
 
 namespace CachingObjectsTool.Editor.Modules
 {
-    public abstract class OverrideObjectPropertyDrawerModule<T> : CustomPropertyDrawerModule
+    public abstract class OverrideObjectPropertyDrawerModule<T> : PropertyDrawer
         where T : ScriptableObject, IValidationObject
     {
         protected abstract string FolderOfCachedOverride { get; }
         
-        List<SerializedProperty> busyProperties = new List<SerializedProperty>();
+        Rect _rect;
+                
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return EditorGUI.GetPropertyHeight(property, label, true);
+        }
         
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            base.OnGUI(position, property, label);
+            WriteRect(position);
             DrawProperty(property, label);
 
+            UniTask.Create(async () =>
             {
-                UniTask.Create(async () =>
-                {
-                    busyProperties.Add(property);
-                    await TryGenerateOverrideNewData(property);
-                    busyProperties.Remove(property);
-                });
-            }
+                await TryGenerateOverrideNewData(property);
+            });
         }
 
         async UniTask TryGenerateOverrideNewData(SerializedProperty property)
@@ -73,5 +72,60 @@ namespace CachingObjectsTool.Editor.Modules
 
             await UniTask.Yield();
         }
+        
+        void WriteRect(Rect rect)
+        {
+            _rect = rect;
+        }
+
+        #region Additional Methods
+
+        protected delegate void DrawPropertyGUI(SerializedProperty property);
+        protected void DrawAsFoldout(SerializedProperty property, GUIContent label, DrawPropertyGUI drawGUI)
+        {
+            var foldoutRect = new Rect(_rect.x, _rect.y, _rect.width, EditorGUIUtility.singleLineHeight);
+            var foldout = EditorGUI.Foldout(foldoutRect, property.isExpanded, label, true);
+            if (foldout != property.isExpanded)
+            {
+                property.isExpanded = foldout;
+            }
+            
+            AddSingleLine();
+            if (property.isExpanded)
+            {
+                EditorGUI.indentLevel++;
+                drawGUI(property);
+                EditorGUI.indentLevel--;
+            }
+        }
+        
+        protected void AddHalfLine()
+        {
+            var modifier = EditorGUIUtility.singleLineHeight * 0.55f;
+            _rect.y += modifier;
+        }
+        
+        protected void AddSingleLine()
+        {
+            var modifier = EditorGUIUtility.singleLineHeight * 1.1f;
+            _rect.y += modifier;
+        }
+        
+        protected void DrawProperty(SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.PropertyField(_rect, property, label, true);
+        }
+
+        protected void DrawHelperBox(string message, MessageType messageType)
+        {
+            AddHalfLine();
+            
+            Rect rect = new Rect(this._rect.x, this._rect.y, this._rect.width, EditorGUIUtility.singleLineHeight);
+            rect.y += EditorGUIUtility.singleLineHeight * 0.55f;
+            
+            EditorGUI.HelpBox(rect, message, messageType);
+        }
+
+        #endregion
     }
 }
